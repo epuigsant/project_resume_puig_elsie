aff596913d824e7601330f8454023c01be206498
Elsie Puig <epuigsant@gmail.com>
2016-04-15 19:25:45 -0700

    add and customize popup plugin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+//Tooltip functionality for profile picture
+
 $( document ).ready( function() {
     $( '.tooltip' ).tooltipster();
 } );
+
+//Modal popup which blurs background and gives additional information
+//when computer icon is clicked on.
+
+$( function() {
+        $( '#show' ).avgrund( {
+            height: 200,
+            holderClass: 'custom',
+            showClose: true,
+            showCloseText: 'Close',
+            enableStackAnimation: true,
+            onBlurContainer: '.container',
+            template: '<p> I am constantly learning ' +
+            'new programming languages and ' +
+            'ways to present data ' +
+            'so check back here for updates.' +
+            'You can always follow me on Twitter and LinkedIn' +
+            'to see recent updates on my progress. </p>'
+        } );
+    } );
